d271a586febb6a08e287f4efda3e0c823c5aff12
pattsang <patrick.tsang@gmail.com>
2013-07-06 22:47:09 -0700

    BLL and DAL
    BusinessLogic Layer and DataAccess Layer

---
+++ BLL/BusinessManager.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using DAL;
+using Jayrock;
+using Jayrock.Json;
+
+namespace BLL
+{
+    public class BusinessManager : BaseHandler
+    {
+        public Int32 InsertBusiness(string name, string businessName, string city)
+        {
+            int businessId = 0;
+            Business bus = new Business();
+            bus.Name = name;
+            bus.BusinessName = businessName;
+            bus.City = city;
+
+            try
+            {
+                Add(bus);
+                businessId = bus.Id;
+            }
+            catch (Exception ex)
+            {
+                //return ex.Message;
+                throw ex; ;
+            }
+
+            return businessId;
+        }
+    }
+}
---
+++ BLL/MemberManager.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using DAL;
+
+namespace BLL
+{
+    public class MemberManager: BaseHandler
+    {
+        public Int32 InsertMember(string firstName, string lastName, string companyName, string email, string password, string paymentToken, string customerToken, string currentSystem)
+        {
+            int memberId = 0;
+
+            if (!IsMemberExisted(firstName, lastName, companyName, email, currentSystem))
+            {
+                Member mem = new Member();
+                mem.FirstName = firstName;
+                mem.LastName = lastName;
+                mem.CompanyName = companyName;
+                mem.Email = email;
+                mem.Password = password;
+                mem.PaymentToken = paymentToken;
+                mem.CustomerToken = customerToken;
+                mem.CurrentSystem = currentSystem;
+
+                try
+                {
+                    Add(mem);
+                    memberId = mem.Id;
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+            }
+
+            return memberId;
+        }
+
+        public bool IsMemberExisted(string firstName, string lastName, string companyName, string emailAddress, string currentSystem)
+        {
+            IQueryable<Member> mem = Repository.Uow.Members.Where(m => m.FirstName == firstName && m.LastName == lastName && m.CompanyName == companyName && m.Email == emailAddress && m.CurrentSystem == currentSystem);
+            if (mem.Count() == 0)
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
